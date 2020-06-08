63fbcab73379c01ded4cce299b38ec855b194386
Mykola Stelnyk <42596628+djkolka@users.noreply.github.com>
2020-06-08 11:43:04 -0500

    Add files via upload

---
+++ mystyle/js/my.js
@@ -0,0 +1,16 @@
+/*
+Theme Name: 2016 Child
+Theme URI: https://stelnyk.com/
+Author: Mykola Stelnyk
+Template: twentysixteen
+Author URI: https://stelnyk.com/
+Description: 2016 Theme
+Version: 1.0.0
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Tags: one-column, two-columns, right-sidebar, accessibility-ready, custom-background, custom-colors, custom-header, custom-menu, editor-style, featured-images, flexible-header, microformats, post-formats, rtl-language-support, sticky-post, threaded-comments, translation-ready, blog
+Text Domain: nobby-child
+
+This theme, like WordPress, is licensed under the GPL.
+Use it to make something cool, have fun, and share what you've learned with others.
+*/
