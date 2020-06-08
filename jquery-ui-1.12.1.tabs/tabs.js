e9d6d28f5b0010a10bd665cfe844d8ae2629cb9a
Mykola Stelnyk <42596628+djkolka@users.noreply.github.com>
2020-06-08 11:39:35 -0500

    Add files via upload

---
+++ jquery-ui-1.12.1.tabs/tabs.js
@@ -0,0 +1,12 @@
+$( "#tabs" ).tabs();
+
+// Hover states on the static widgets
+$( "#dialog-link, #icons li" ).hover(
+	function() {
+		$( this ).addClass( "ui-state-hover" );
+	},
+	function() {
+		$( this ).removeClass( "ui-state-hover" );
+	}
+);
+
