fe95ea362bc1f10f073ce116ef1e6f1a86dcc5b2
Mykola Stelnyk <42596628+djkolka@users.noreply.github.com>
2020-09-11 11:23:55 -0500

    Update add_bootstrap.php

--- add_bootstrap.php
+++ add_bootstrap.php
@@ -20,6 +20,7 @@ function add_bootstrap(){
   ?>
 
   <input type="radio" name="option_name[bootstrap_version]" id="bootstrap_version" value="0"<?php if ($val == 0)echo 'checked="checked"';?>>No Bootstrap<br>
+  <input type="radio" name="option_name[bootstrap_version]" id="bootstrap_version" value="452"<?php if ($val == 452)echo 'checked="checked"';?>>Version-4.5.2<br>
   <input type="radio" name="option_name[bootstrap_version]" id="bootstrap_version" value="4"<?php if ($val == 4)echo 'checked="checked"';?>>Version-4.3.1<br>
   <input type="radio" name="option_name[bootstrap_version]" id="bootstrap_version" value="3"<?php if ($val == 3)echo 'checked="checked"';?>>Version-3.3.7<br>
   <?php
@@ -31,22 +32,32 @@ function bootstrap_callback() {
 
 $val = get_option('option_name');
 $val = $val ? $val['bootstrap_version'] : null;
-if ($val == 4) {
-
-// *** add bootstrap-4.3.1 ***
-function register_style_and_scripts_bootstrap_431() {
-    // false - header, true - footer
-    wp_enqueue_style( 'bootstrap_431_css', plugin_dir_url( __FILE__ ) . 'bootstrap/bootstrap-4.3.1-dist/css/bootstrap.min.css' );
-    wp_enqueue_script( 'bootstrap_431_jquery_js', plugin_dir_url( __FILE__ ) . 'bootstrap/bootstrap-4.3.1-dist/js/jquery-3.3.1.slim.min.js', null, null, false );
-    wp_enqueue_script( 'bootstrap_431_popper_js', plugin_dir_url( __FILE__ ) . 'bootstrap/bootstrap-4.3.1-dist/js/popper.min.js', null, null, false );
-    wp_enqueue_script( 'bootstrap_431_js', plugin_dir_url( __FILE__ ) . 'bootstrap/bootstrap-4.3.1-dist/js/bootstrap.min.js', null, null, false );
-    wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css');
-  
-}
-add_action( 'wp_enqueue_scripts', 'register_style_and_scripts_bootstrap_431' );
-// END *** add bootstrap-4.3.1 ***
-
-} else if ($val == 3){
+if ($val == 452) {
+  // *** add bootstrap-4.5.2 ***
+  function register_style_and_scripts_bootstrap_452() {
+      // false - header, true - footer
+      wp_enqueue_style( 'bootstrap_452_css', plugin_dir_url( __FILE__ ) . 'bootstrap/bootstrap-4.5.2-dist/css/bootstrap.min.css' );
+      wp_enqueue_script( 'bootstrap_452_js', plugin_dir_url( __FILE__ ) . 'bootstrap/bootstrap-4.5.2-dist/js/bootstrap.min.js', null, null, false );
+      wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css');
+  }
+  add_action( 'wp_enqueue_scripts', 'register_style_and_scripts_bootstrap_452' );
+  // END *** add bootstrap-4.5.2 ***
+} 
+else if ($val == 4) {
+  // *** add bootstrap-4.3.1 ***
+  function register_style_and_scripts_bootstrap_431() {
+      // false - header, true - footer
+      wp_enqueue_style( 'bootstrap_431_css', plugin_dir_url( __FILE__ ) . 'bootstrap/bootstrap-4.3.1-dist/css/bootstrap.min.css' );
+      wp_enqueue_script( 'bootstrap_431_jquery_js', plugin_dir_url( __FILE__ ) . 'bootstrap/bootstrap-4.3.1-dist/js/jquery-3.3.1.slim.min.js', null, null, false );
+      wp_enqueue_script( 'bootstrap_431_popper_js', plugin_dir_url( __FILE__ ) . 'bootstrap/bootstrap-4.3.1-dist/js/popper.min.js', null, null, false );
+      wp_enqueue_script( 'bootstrap_431_js', plugin_dir_url( __FILE__ ) . 'bootstrap/bootstrap-4.3.1-dist/js/bootstrap.min.js', null, null, false );
+      wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css');
+    
+  }
+  add_action( 'wp_enqueue_scripts', 'register_style_and_scripts_bootstrap_431' );
+  // END *** add bootstrap-4.3.1 ***
+} 
+else if ($val == 3){
 
 // *** add bootstrap-3.3.7 ***
 function register_style_and_scripts_bootstrap_337() {
